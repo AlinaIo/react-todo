ad88a34a215f495a740173d46919a6835acc5834
IonescuA <alina.ionescu@haufe-lexware.com>
2017-08-30 17:57:01 +0300

    add filter functionality

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -26,7 +26,7 @@ describe('TodoAPI', () => {
         });
 
         it('should not set invalid todos array', () => {
-            var badTodos = {a: 'b'};
+            var badTodos = { a: 'b' };
             TodoAPI.setTodos(badTodos);
 
             expect(localStorage.getItem('todos')).toBe(null);
@@ -50,6 +50,53 @@ describe('TodoAPI', () => {
             var actualTodos = TodoAPI.getTodos();
 
             expect(actualTodos).toEqual(todos);
+        });
+    });
+
+    describe('filterTodos', () => {
+        var todos = [{
+            id: 1,
+            text: 'some text',
+            completed: true
+        },
+        {
+            id: 2,
+            text: 'other text',
+            completed: false
+        },
+        {
+            id: 3,
+            text: 'other other text',
+            completed: true
+        }];
+
+        it('should return all items if showCompleted is true', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos,true,'');
+
+            expect(filteredTodos.length).toBe(3);
+        });
+
+        it('should return all items if showCompleted is false', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos,false,'');
+
+            expect(filteredTodos.length).toBe(1);
+        });
+
+        it('should sort by completed status', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+            expect(filteredTodos[0].completed).toEqual(false);
+        })
+
+        it('should filter todos by searchText', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos,true,'some');
+
+            expect(filteredTodos.length).toBe(2);
+        });
+
+        it('should return all todos if searchText is empty', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos,true,'');
+
+            expect(filteredTodos.length).toBe(3);
         });
     });
 });
