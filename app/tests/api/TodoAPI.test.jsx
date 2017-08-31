93249445df9c21534165148124dcc1bbd8a707ff
IonescuA <alina.ionescu@haufe-lexware.com>
2017-08-31 19:47:24 +0300

    add completed and created timestamps

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -90,7 +90,7 @@ describe('TodoAPI', () => {
         it('should filter todos by searchText', () => {
             var filteredTodos = TodoAPI.filterTodos(todos,true,'some');
 
-            expect(filteredTodos.length).toBe(2);
+            expect(filteredTodos.length).toBe(1);
         });
 
         it('should return all todos if searchText is empty', () => {
