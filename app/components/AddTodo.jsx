50cd22511d18737a672d53af9045da320e186326
IonescuA <alina.ionescu@haufe-lexware.com>
2017-09-01 17:56:38 +0300

    Style Todo application

--- app/components/AddTodo.jsx
+++ app/components/AddTodo.jsx
@@ -4,7 +4,7 @@ var AddTodo = React.createClass({
     onFormSubmit: function (e) {
         e.preventDefault();
         var text = this.refs.myTodo.value;
-        if (text !== '') {
+        if (text.length >0) {
             this.refs.myTodo.value = '';
             this.props.onAddTodo(text);
         } else {
@@ -13,10 +13,10 @@ var AddTodo = React.createClass({
     },
     render: function () {
         return (
-            <div>
+            <div className="container__footer">
                 <form onSubmit={this.onFormSubmit}>
                     <input type="text" ref="myTodo" placeholder="enter what you want to do" />
-                    <button className="button hollow">Add Todo</button>
+                    <button className="button expanded">Add Todo</button>
                 </form>
             </div>
         )
--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -9,7 +9,7 @@ var TodoAPI = require('TodoAPI');
 
 
 var TodoApp = React.createClass({
-    getInitialState: function() {
+    getInitialState: function () {
         return {
             showCompleted: false,
             searchText: '',
@@ -21,16 +21,16 @@ var TodoApp = React.createClass({
     },
     handleAddTodo: function (text) {
         this.setState({
-           todos: [
-               ...this.state.todos,
-               {
-                   id: uuid(),
-                   text: text,
-                   completed: false,
-                   createdAt: moment().unix(),
-                   completedAt: undefined
-               }
-           ] 
+            todos: [
+                ...this.state.todos,
+                {
+                    id: uuid(),
+                    text: text,
+                    completed: false,
+                    createdAt: moment().unix(),
+                    completedAt: undefined
+                }
+            ]
         });
     },
     handleToggle: function (id) {
@@ -41,7 +41,7 @@ var TodoApp = React.createClass({
             }
             return todo;
         });
-        this.setState({todos: updatedTodos});
+        this.setState({ todos: updatedTodos });
     },
     handleSearch: function (showCompleted, searchText) {
         this.setState({
@@ -50,13 +50,20 @@ var TodoApp = React.createClass({
         })
     },
     render: function () {
-        var {todos, showCompleted, searchText} = this.state;
-        var filteredTodos = TodoAPI.filterTodos(todos,showCompleted,searchText);
+        var { todos, showCompleted, searchText } = this.state;
+        var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
         return (
             <div>
-                <TodoSearch onSearch={this.handleSearch}/>
-                <TodoList todos={filteredTodos} onToggle={this.handleToggle}/>
-                <AddTodo onAddTodo={this.handleAddTodo}/>
+                <h1 className="page-title">Todo App</h1>
+                <div className="row">
+                    <div className="column small-centered small-11 medium-6 large-5">
+                        <div className="container">
+                            <TodoSearch onSearch={this.handleSearch} />
+                            <TodoList todos={filteredTodos} onToggle={this.handleToggle} />
+                            <AddTodo onAddTodo={this.handleAddTodo} />
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
